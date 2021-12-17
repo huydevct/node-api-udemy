53be45d406c9e56e7f4eca3dfdeb7f6d9f83428a
huydevct <huyctdev@example.com>
2021-12-18 00:17:27 +0700

    working with express

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
-const http = require('http');
+const express = require("express");
+const path = require("path");
 
-const server = http.createServer((req,res) => {
-    console.log(req);
-    // process.exit();
+const app = express();
+
+const adminRouter = require("./routes/admin");
+const shopRouter = require("./routes/shop");
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use("/admin", adminRouter);
+app.use(shopRouter);
+
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
 
-server.listen(3000);
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
---
+++ routes/shop.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const path = require("path");
+
+const rootDir = require("../util/path");
+
+const router = express.Router();
+
+router.use("/shop", (req, res, next) => {
+  res.sendFile(path.join(rootDir, "views", "shop.html"));
+});
+
+module.exports = router;
